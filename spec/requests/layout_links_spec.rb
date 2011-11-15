d707db1d2b8c5db9257bad2f1d0bdbd060015d5b
Frank J. Puga <fpuga@cs.utsa.edu>
2011-11-15 10:43:26 -0600

    added paths to pages

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe "LayoutLinks" do
-  render_views
+
     it "should have a home page at '/'" do
       get '/'
       response.should have_selector("title", :content => "Kays App | Home")
